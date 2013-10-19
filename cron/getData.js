e496dc15fef6413f6743f9fbdc14b0afcaf432f6
airyland <airyland@qq.com>
2013-10-19 15:16:07 +0800

    automatically get lastes date

--- cron/getData.js
+++ cron/getData.js
@@ -1,4 +1,11 @@
  var fs = require('fs');
+ // 日期格式化
+ var format = function(date) {
+   var month = (date.getMonth() + 1) + '',
+     day = date.getDate() + '',
+     rs = date.getFullYear() + '-' + (month.length === 1 ? '0' + month : month) + '-' + (day.length === 1 ? '0' + day : day);
+   return rs;
+ };
  var httpGet = function(url, callback) {
    var http = require('http');
    http.get(url, function(rs) {
@@ -7,10 +14,23 @@
        data += chunk;
      });
      rs.on('end', function() {
-       callback(data);
+       callback(JSON.parse(data));
      });
-   })
- }
+   });
+ };
+
+ // 取得几天前数据 
+ var daysAgo = function(day, date, isFormat) {
+   var today;
+   if (date) {
+     today = +new Date(date);
+   } else {
+     today = +new Date();
+   }
+   var offset = day * 24 * 3600 * 1000;
+   if (isFormat && isFormat === false) return new Date(today - offset);
+   return format(new Date(today - offset));
+ };
 
  httpGet('http://106.3.38.38:8888/api/app.json?type=3month_install', function(data) {
    console.log('monthly install fetched successfully');
@@ -36,7 +56,7 @@
  ['', 'organic', 'referral', '(none)'].forEach(function(one) {
    var option = {
      "metrics": "ga:visits",
-     "end-date": "2013-10-11",
+     "end-date": daysAgo(2),
      "start-date": "2013-06-15",
      "dimensions": "ga:date",
      "ids": "ga:644519",
