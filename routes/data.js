3b4fabfefdc1c2acae562362b8118fb4e690787d
airyland <airyland@qq.com>
2013-11-07 21:01:13 +0800

    add create info and owners info to data

--- routes/data.js
+++ routes/data.js
@@ -1,24 +1,47 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
 // 数据源
 var Data = mongoose.model('Data', {
     name: String,
     type: String, //数据类型，ga || umeng || seedit,
     chartType: String, // 图表类型
-    option: mongoose.Schema.Types.Mixed // 选项为复杂类型
+    option: mongoose.Schema.Types.Mixed, // 选项为复杂类型
+    // when
+    create_at: {
+        type: Date,
+        default: Date.now
+    },
+    // author
+    create_by: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    // owners
+    owners: Array
 });
 
 exports.data = {
     list: function(req, res) {
-        Data.find({}, function(err, data) {
-            res.send({
-                error: 0,
-                data: data
+        Data.find({}, null, {
+                sort: {
+                    date: -1
+                }
+            },
+            function(err, data) {
+                res.send({
+                    error: 0,
+                    data: data
+                });
             })
-        })
     },
     add: function(req, res) {
+        // add author info
+        req.body.create_by = req.user.uid;
+        // add default owners
+        if (!req.body.owners) {
+            req.body.owners = [req.user.uid];
+        }
         var data = new Data(req.body);
-
         data.save(function(err, item) {
             if (err) throw err;
             res.send({
