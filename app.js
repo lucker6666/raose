9a1f617d77f93260569daf53ca3c3a34198d44c5
airyland <airyland@qq.com>
2013-11-22 10:20:53 +0800

    bug fix

--- app.js
+++ app.js
@@ -596,7 +596,7 @@ app.post('/api/ga.json', function(req, res) {
             // add log
             api.log.add({
               type: 'getData',
-              operator: req.user.uid ? req.user.uid : 'null',
+              operator: (req.user && req.user.uid) ? req.user.uid : 'null',
               details: {
                 filters: req.query.filters
               }
