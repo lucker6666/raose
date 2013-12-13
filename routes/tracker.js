33b56e2fe29bf119d150a3d3c1a0dfbdb13bf1d9
airyland <airyland@qq.com>
2013-12-13 19:16:28 +0800

    update tracker

--- routes/tracker.js
+++ routes/tracker.js
@@ -2,6 +2,9 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var Track = mongoose.model('usertrack', {
+    // visitor id
+    uuid: String,
+    // website uid
     uid: Number,
     // action name
     action: String,
@@ -16,8 +19,33 @@ var Track = mongoose.model('usertrack', {
     }
 });
 
+// generate uuid
+var createUUID = function () {
+    // http://www.ietf.org/rfc/rfc4122.txt
+    var s = [];
+    var hexDigits = "0123456789abcdef";
+    for (var i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[8] = s[13] = s[18] = s[23] = "-";
+
+    var uuid = s.join("");
+    return uuid;
+};
+
 module.exports = {
-    track: function(req, res) {
+    track: function (req, res) {
+        // get cookie
+        var uuid;
+        if (req.cookies.uuid) {
+            uuid = req.cookies.uuid;
+        } else {
+            uuid = createUUID();
+            res.cookie('uuid', uuid, {maxAge: 10 * 365 * 24 * 3600 * 1000});
+        }
+
         var query = req.query;
         var datas = {
             uid: parseInt(req.query.uid, 10),
@@ -25,11 +53,14 @@ module.exports = {
             actionDetails: req.query.actionDetails,
             clientDetails: req.query.clientDetails
         };
+
+        datas.uuid = uuid;
+
         if (!datas.clientDetails) datas.clientDetails = {};
-        datas.clientDetails.ip = req.ip
+        datas.clientDetails.ip = req.ip;
         var ip = req.ip;
         var track = new Track(datas);
-        track.save(function(err) {
+        track.save(function (err) {
             if (err) throw err;
             res.send(204);
         });
