0a9faf3e724475ec19a1be07e3809cec6fa613e7
airyland <airyland@qq.com>
2014-01-18 09:57:40 +0000

    normal update

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,7 +1,8 @@
+var util = require('util');
 module.exports = function(err, req, res, next) {
   if (!err) return next();
   res.send({
-    error: 500,
-    msg: err
+    error:1002,
+    msg:util.inspect(err)
   });
 };
