559fcfcdc864653200d7c084ff3113fa64a8ad56
airyland <airyland@qq.com>
2014-01-25 08:30:36 +0000

    close mongoose connection when process exit

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ server.start();
 var kue = require('kue');
 kue.app.listen(3000);
 console.log('start kue server on port 3000');
+
+// close mongoose connection
+process.on('SIGINT', function() {
+    var mongoose = require('./lib/mongoose');
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through app termination');
+        process.exit(0);
+    });
+});
