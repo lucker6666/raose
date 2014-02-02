3037b13cab72522c0757913a7fa9b4276d67a097
airyland <airyland@qq.com>
2014-02-03 00:18:15 +0800

    add Token Model

---
+++ models/Token.js
@@ -0,0 +1,23 @@
+var mongoose = require('./lib/mongoose');
+var tokenSchema = mongoose.Schema({
+    app_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'App',
+        require: true
+    },
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        require: true,
+    },
+    token: {
+        type: String
+        require: true
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('token', tokenSchema);
