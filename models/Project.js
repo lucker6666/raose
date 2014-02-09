8434f74c4cdca1550e1fbd73165c6cb74c70b424
airyland <airyland@qq.com>
2014-02-09 07:43:38 +0000

    add tasklist Model

--- models/Project.js
+++ models/Project.js
@@ -1,6 +1,47 @@
 var mongoose = require('../lib/mongoose'),
     Schema = mongoose.Schema;
 
+// tasklist Schema
+var tasklistSchema = Schema({
+  // list name
+  name:{
+    type: String,
+    required: true
+  },
+  // list description
+  note:String,
+  // workspace id
+  workspace_id:{
+    type: Schema.Types.ObjectId,
+    ref: 'Project',
+    required: true
+  },
+  // task number
+  tasks_count:{
+    type: Number,
+    default: 0
+  },
+  // created by
+  created_by:{
+     type: Schema.Types.ObjectId,
+     ref: 'User',
+  },
+  created_at:{
+    type: Date,
+    default: Date.now
+  },
+  updated_by:{
+     type: Schema.Types.ObjectId,
+     ref: 'User',
+  },
+  updated_at: Date,
+  archived: {
+    type: Boolean,
+    default: false
+  },
+  archived_at: Date
+});
+
 var ProjectSchema = Schema({
     // project name
     name: {
@@ -22,16 +63,19 @@ var ProjectSchema = Schema({
         type: Boolean,
         default: false
     },
+    archived_at: Date,
     // followers
     followers: Array,
     // modified date
-    updated_at: Date,
+    modified_at: Date,
     // modified by
-    updated_by: {
+    modified_by: {
         type: Schema.Types.ObjectId,
         ref: 'User'
     },
     color: String,
+    // task list
+    tasklist: [tasklistSchema],
     // cached metas
     metas: {
         tasks: {
